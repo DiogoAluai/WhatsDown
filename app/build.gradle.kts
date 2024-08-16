2c5b0ea70b2c69428365cc1c30faacd45fbdcfda
Diogo Aluai <diogoaluai@gmail.com>
2024-08-16 18:43:17 +0100

    Upgrade to version 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "daluai.app.whatsdown"
         minSdk = 27
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
